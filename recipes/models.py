5d9cea4f08187085e84319a7fe8f098bef4fe8f3
juan Henrique <henriquejuan36@gmail.com>
2022-09-15 09:42:21 -0300

    sss

--- recipes/models.py
+++ recipes/models.py
@@ -1,14 +1,19 @@
 from distutils.command.upload import upload
+from unicodedata import category
 
+from django.contrib.auth.models import User
 from django.db import models
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
 
 # Create your models here.
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
     slug = models.SlugField()
-    preparation_time = models.models.IntegerField()
+    preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
     serving = models.IntegerField()
     serving_unit = models.CharField(max_length=65)
@@ -18,3 +23,5 @@ class Recipe(models.Model):
     uptade_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL,null=True)
