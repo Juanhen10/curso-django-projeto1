49ba45a85c52fbd29312d26cb633d9bd6f79eb0a
juan Henrique <henriquejuan36@gmail.com>
2022-09-19 09:33:29 -0300

    ss

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import Category, Juan, Recipe
+
+
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+admin.site.register(Category, CategoryAdmin)    
+
+
+class RecipesAdmin(admin.ModelAdmin):
+    ...
+admin.site.register(Recipe,RecipesAdmin)
+
+
+
